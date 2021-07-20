5f061174f20b0d9d9e78742bb088d6c7fa6bfc4d
poojadas07 <poojadas04kv@gmail.com>
2021-07-20 23:47:25 +0530

    modules added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { LoginComponent } from './login/login.component';
 import { ForgotPasswordComponent } from './login/forgot-password/forgot-password.component';
 import { RegisterComponent } from './register/register.component';
-import { HomeDetailsComponent } from './home-details/home-details.component';
+import { AuthGuard } from './guards/auth.guard';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     ForgotPasswordComponent,
-    RegisterComponent,
-    HomeDetailsComponent
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

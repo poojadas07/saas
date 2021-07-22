26132f74cdb06265a471e395436a3ceb1890c76e
poojadas07 <poojadas04kv@gmail.com>
2021-07-22 23:49:03 +0530

    sub routing added

--- src/app/home-details/home-details-routing.module.ts
+++ src/app/home-details/home-details-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
+import { HomeDetailsComponent } from './home-details.component';
 
-const routes: Routes = [];
+const routes: Routes =  [  {path: '', component: HomeDetailsComponent, children: [
+                          {path: '', pathMatch: 'full', redirectTo: 'home'},
+                          {path: 'home',  component: ContactComponent}]}
+                        ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class HomeDetailsRoutingModule { }

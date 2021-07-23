4cc55638dbb68e31ff9195288e43779b77037eec
poojadas07 <poojadas04kv@gmail.com>
2021-07-23 23:49:39 +0530

    sub modules added

--- src/app/home-details/home-details.module.ts
+++ src/app/home-details/home-details.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
+import { HomeDetailsComponent } from './home-details.component';
+import { ContactComponent } from './contact/contact.component';
+import { DonateComponent } from './donate/donate.component';
+import { RecieveComponent } from './recieve/recieve.component';
+import { FoodMapComponent } from './food-map/food-map.component';
+import { HomeDetailsRoutingModule } from './home-details-routing.module';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeDetailsComponent,
+    ContactComponent,
+    DonateComponent,
+    RecieveComponent,
+    FoodMapComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    HomeDetailsRoutingModule
   ]
 })
 export class HomeDetailsModule { }

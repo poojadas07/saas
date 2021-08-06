50fa389aba6dce1b9ef7fa2ad81d65c00c579179
poojadas07 <poojadas04kv@gmail.com>
2021-08-06 08:49:38 +0530

    Recieve component

---
+++ src/app/home-details/recieve/recieve.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RecieveComponent } from './recieve.component';
+
+describe('RecieveComponent', () => {
+  let component: RecieveComponent;
+  let fixture: ComponentFixture<RecieveComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ RecieveComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RecieveComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/home-details/recieve/recieve.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recieve',
+  templateUrl: './recieve.component.html',
+  styleUrls: ['./recieve.component.css']
+})
+export class RecieveComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

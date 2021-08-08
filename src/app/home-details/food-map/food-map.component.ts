ef00f8d77c38cfa6476f13823b05a3fe9d442ad2
poojadas07 <poojadas04kv@gmail.com>
2021-08-08 08:52:06 +0530

    map component

---
+++ src/app/home-details/food-map/food-map.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-food-map',
+  templateUrl: './food-map.component.html',
+  styleUrls: ['./food-map.component.css']
+})
+export class FoodMapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

4f3c8102d970b2911a7f518b93e06a4c3b72d62c
poojadas07 <poojadas04kv@gmail.com>
2021-08-28 22:26:48 +0530

    more routing added

--- src/app/home-details/home-details-routing.module.ts
+++ src/app/home-details/home-details-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContactComponent } from './contact/contact.component';
+import { DonateComponent } from './donate/donate.component';
+import { FoodMapComponent } from './food-map/food-map.component';
 import { HomeDetailsComponent } from './home-details.component';
+import { HomeComponent } from './home/home.component';
+import { RecieveComponent } from './recieve/recieve.component';
 
 const routes: Routes =  [  {path: '', component: HomeDetailsComponent, children: [
                           {path: '', pathMatch: 'full', redirectTo: 'home'},
-                          {path: 'home',  component: ContactComponent}]}
+                          {path: 'home',  component: HomeComponent},
+                          {path: 'donate',  component: DonateComponent},
+                          {path: 'recieve',  component: RecieveComponent},
+                          {path: 'food-map',  component: FoodMapComponent},
+                          {path: 'contact',  component: ContactComponent}]}
                         ];
 
 @NgModule({

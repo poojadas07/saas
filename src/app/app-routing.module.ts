1cfc7b9b4b28fa45cb52a973a1840966f2542c2c
poojadas07 <poojadas04kv@gmail.com>
2021-08-30 00:26:31 +0530

    routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './guards/auth.guard';
+import { ForgotPasswordComponent } from './login/forgot-password/forgot-password.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [{path : '', pathMatch: 'full', redirectTo: '/register'},
                         { path: 'dashboard', loadChildren: () => import('./home-details/home-details.module').then(m => m.HomeDetailsModule)},
                         { path: 'login', component: LoginComponent},
-                        { path: 'register', component: RegisterComponent}
+                        { path: 'register', component: RegisterComponent},
+                        { path: 'forgot', component: ForgotPasswordComponent}
                       ];
 
 @NgModule({
